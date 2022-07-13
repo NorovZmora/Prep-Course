7afbd304df11ac414b03faa5042eed6a44e68218
NorovZmora <norbinyeah@gmail.com>
2022-07-12 18:44:30 -0600

    other progress

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -76,7 +76,6 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código: 
-  var str1 = ''
   
 }
 
